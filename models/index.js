3a6792598cc1f0d4ab4acfe835c8f6d96850bac9
wms09291963 <98226872+wms09291963@users.noreply.github.com>
2023-05-09 13:34:50 -0400

    added connect to mongo db

--- models/index.js
+++ models/index.js
@@ -1,10 +1,27 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema()
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
 
-module.exports.Place = require('./places')
-module.exports.Comment = require('./comment')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(function (error) {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+
+
+//export const Place = require('./places')
+//export const Comment = require('./comment')
+
+module.exports = {
+  Place: require('./places'),
+  Comment: require('./comment')
+};
